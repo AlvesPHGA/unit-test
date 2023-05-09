be9936f1a968bed25dfa5d242602102654e93e95
Pedro H <pedrohenriquegomesalves@gmail.com>
2023-05-09 11:08:43 -0300

    updated App.spec, was added a new test to buttom component

--- src/App.spec.tsx
+++ src/App.spec.tsx
@@ -1,12 +1,35 @@
 import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
-test('sum', () => {
-   const { getByText } = render(<App />);
+// test('sum', () => {
+//    const { getByText } = render(<App />);
 
-   // expect(getByText('Stating in Unit testing')).toBeInTheDocument();
-   expect(getByText('Stating in Unit testing')).toHaveAttribute(
-      'class',
-      'text',
-   );
+//    // expect(getByText('Stating in Unit testing')).toBeInTheDocument();
+//    expect(getByText('Stating in Unit testing')).toHaveAttribute(
+//       'class',
+//       'text',
+//    );
+// });
+
+describe('App component', () => {
+   it('should render list items', () => {
+      const { getByText } = render(<App />);
+
+      expect(getByText('Jonnas')).toBeInTheDocument();
+      expect(getByText('Julio')).toBeInTheDocument();
+      expect(getByText('Judas')).toBeInTheDocument();
+   });
+
+   it('should be able to add new item to the list', async () => {
+      const { getByText, debug } = render(<App />);
+
+      const addButton = getByText('Adicionar');
+
+      debug();
+      await userEvent.click(addButton);
+      debug();
+
+      expect(getByText('Jako')).toBeInTheDocument();
+   });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
+import React from 'react';
+
 function App() {
+   const [list, setList] = React.useState(['Jonnas', 'Julio', 'Judas']);
+
+   function handleList() {
+      setList((states) => [...states, 'Jako']);
+   }
    return (
       <>
          <h1 className="text">Stating in Unit testing</h1>
-         <p>Jonas</p>
-         <p data-testid="rug">Jeppedo</p>
-         <p>Judas</p>
-         <p>Jaco</p>
+
+         <button onClick={handleList}>Adicionar</button>
+         <ul>
+            {list.map((item) => (
+               <li key={item}>{item}</li>
+            ))}
+         </ul>
       </>
    );
 }

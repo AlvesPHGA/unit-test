f092d487b7941e6b682c85a353fe118ba1a056b8
Pedro H <pedrohenriquegomesalves@gmail.com>
2023-05-09 10:26:51 -0300

    updated test

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,10 @@ function App() {
    return (
       <>
          <h1 className="text">Stating in Unit testing</h1>
+         <p>Jonas</p>
+         <p data-testid="rug">Jeppedo</p>
+         <p>Judas</p>
+         <p>Jaco</p>
       </>
    );
 }

628a279fd2cca6a8ea0326f57c57aaae92d64bee
Pedro H <pedrohenriquegomesalves@gmail.com>
2023-05-09 11:13:15 -0300

    updated app, was added a input

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 
 function App() {
+   const [newItem, setNewItem] = React.useState('');
    const [list, setList] = React.useState(['Jonnas', 'Julio', 'Judas']);
 
    function handleList() {
-      setList((states) => [...states, 'Jako']);
+      setList((states) => [...states, newItem]);
    }
    return (
       <>
          <h1 className="text">Stating in Unit testing</h1>
 
+         <input
+            type="text"
+            value={newItem}
+            onChange={(e) => setNewItem(e.target.value)}
+         />
          <button onClick={handleList}>Adicionar</button>
          <ul>
             {list.map((item) => (

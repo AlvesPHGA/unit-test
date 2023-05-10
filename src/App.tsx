9384a64c81a5048ecfaf5bf32844b454e6113a50
Pedro H <pedrohenriquegomesalves@gmail.com>
2023-05-10 11:47:14 -0300

    updated appltsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,7 @@
-import React from 'react';
+import List from './component/list';
 
 function App() {
-   const [newItem, setNewItem] = React.useState('');
-   const [list, setList] = React.useState(['Jonnas', 'Julio', 'Judas']);
-
-   function handleList() {
-      setTimeout(() => {
-         setList((states) => [...states, newItem]);
-      }, 500);
-   }
-
-   function handleRemoveItem(item: string) {
-      setTimeout(() => {
-         setList((states) => states.filter((i) => i !== item));
-      }, 500);
-   }
-   return (
-      <>
-         <h1 className="text">Stating in Unit testing</h1>
-
-         <input
-            placeholder="Type new item"
-            type="text"
-            value={newItem}
-            onChange={(e) => setNewItem(e.target.value)}
-         />
-         <button onClick={handleList}>Adicionar</button>
-         <ul>
-            {list.map((item) => (
-               <li key={item}>
-                  {item}
-                  <button onClick={() => handleRemoveItem(item)}>Remove</button>
-               </li>
-            ))}
-         </ul>
-      </>
-   );
+   return <List initialItems={['Jonnas', 'Junim', 'Costa']} />;
 }
 
 export default App;
